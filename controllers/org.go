330d74bc6fa63a4084fce22a18b009380fc1843b
tangshuhan214 <519176197@qq.com>
2019-09-26 14:42:54 +0800

    111

--- controllers/org.go
+++ controllers/org.go
@@ -11,9 +11,9 @@ type OrgControllers struct {
 	beego.Controller
 }
 
-func (this *OrgControllers) ActionFunc() {
-	action := this.Ctx.Input.Param(":action")
-	data := this.Ctx.Input.RequestBody
+func (init *OrgControllers) ActionFunc() {
+	action := init.Ctx.Input.Param(":action")
+	data := init.Ctx.Input.RequestBody
 	params := map[string]interface{}{}
 	_ = json.Unmarshal(data, &params)
 
@@ -22,10 +22,10 @@ func (this *OrgControllers) ActionFunc() {
 		go business.GetOrgInfo(params, c)
 	} else if action == "InsertOrgInfo" {
 		var orgInfo models.POrgInfo
-		json.Unmarshal(this.Ctx.Input.RequestBody, &orgInfo)
+		_ = json.Unmarshal(init.Ctx.Input.RequestBody, &orgInfo)
 		go business.InsertOrUpdateOrgInfo(&orgInfo, c)
 	}
 	resp := <-c
-	this.Data["json"] = resp
-	this.ServeJSON()
+	init.Data["json"] = resp
+	init.ServeJSON()
 }

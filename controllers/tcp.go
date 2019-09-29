841a14d5c93d89b598a97813e1b92bf04ea74954
tangshuhan214 <519176197@qq.com>
2019-09-29 11:14:54 +0800

    111

--- controllers/tcp.go
+++ controllers/tcp.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/axgle/mahonia"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
 	"net"
 	"os"
 )
@@ -34,16 +38,14 @@ func (tcp *TcpControllers) TcpOnline() {
 	}
 
 	fmt.Println("connection success")
-	sender(conn)
+	resp := sender(conn)
 	fmt.Println("send over")
-	tcp.Ctx.WriteString("SUCCESS")
+	tcp.Ctx.WriteString(resp)
 }
 
-func sender(conn *net.TCPConn) {
+func sender(conn *net.TCPConn) (resp string) {
 	words := "000144{\"channelno\":\"1\",\"transtype\":\"1\",\"countno\":\"042012\",\"terminal_serialno\":\"11620170426052342501\",\"amount\":\"1.00\",\"auth_code\":\"130227498519276010\"}"
 	gbk := mahonia.NewEncoder("gbk").ConvertString(words)
-	bytes := []byte(gbk)
-	fmt.Print(bytes)
 	msgBack, err := conn.Write([]byte(gbk)) //给服务器发信息
 
 	if err != nil {
@@ -52,7 +54,9 @@ func sender(conn *net.TCPConn) {
 	}
 	buffer := make([]byte, 1024)
 	msg, err := conn.Read(buffer) //接受服务器信息
-	result := mahonia.NewEncoder("GBK").ConvertString(string(buffer[:msg]))
+	data, _ := ioutil.ReadAll(transform.NewReader(bytes.NewReader(buffer[:msg]), simplifiedchinese.GBK.NewEncoder()))
+	result := mahonia.NewEncoder("GBK").ConvertString(string(data))
 	fmt.Println(conn.RemoteAddr().String(), "服务器反馈：", result, msgBack, "；实际发送了", len(words))
 	_, _ = conn.Write([]byte("ok")) //在告诉服务器，它的反馈收到了。
+	return result
 }

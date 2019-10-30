ba128e0cbc2ff71367480c9c10724227a5b3095b
tangshuhan214 <519176197@qq.com>
2019-10-30 14:49:37 +0800

    23123111

--- common/slice2map.go
+++ common/slice2map.go
@@ -25,7 +25,7 @@ func StructSlice2Map(list interface{}) []map[string]interface{} {
 		jsonBytes, _ := json.Marshal(obj)          //将结构体转为JSON字符串
 		var only map[string]interface{}            //这里做两次转换的原因是去除首字母大写
 		only = createJsonUseNum(string(jsonBytes)) //将JSON字符串转为Map[string]interface{}
-		for key, value := range only { //判断该属性是否为json.Number,如果是调用转换方法
+		for key, value := range only {             //判断该属性是否为json.Number,如果是调用转换方法
 			v := reflect.ValueOf(value)
 			of := reflect.TypeOf(value) //使用断言机制判断当前传入类型
 			if of.Name() == "Number" {

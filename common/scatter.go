a94f60a633889c1e6ec78d8d3be6e632656461d8
tangshuhan214 <519176197@qq.com>
2020-01-14 09:54:34 +0800

    132123123

--- common/scatter.go
+++ common/scatter.go
@@ -6,11 +6,6 @@ import (
 	"sync"
 )
 
-type FiberSliceUtils struct {
-	channel     chan interface{}
-	resultSlice []interface{}
-}
-
 func NewScatterSlice(data interface{}, do func(todo interface{}) interface{}) []interface{} {
 	v := reflect.ValueOf(data) //使用断言机制判断当前传入类型
 	if v.Kind() != reflect.Slice {
@@ -25,10 +20,10 @@ func NewScatterSlice(data interface{}, do func(todo interface{}) interface{}) []
 		ret[i] = v.Index(i).Interface()
 	}
 
-	s := &FiberSliceUtils{
-		channel:     make(chan interface{}, len(ret)),
-		resultSlice: make([]interface{}, 0),
-	}
+	//并发安全通道，用于保存处理完毕的数据体
+	channel := make(chan interface{}, len(ret))
+	//用于返回的数据切片
+	resultSlice := make([]interface{}, 0)
 
 	for _, v := range ret {
 		go func(todo interface{}) {
@@ -40,21 +35,21 @@ func NewScatterSlice(data interface{}, do func(todo interface{}) interface{}) []
 			//函数式接口异步处理切片内数据
 			resp := do(todo)
 			//装入并发安全通道
-			s.channel <- resp
+			channel <- resp
 		}(v)
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(cap(s.channel))
+	wg.Add(cap(channel))
 	go func() {
 		for {
 			select {
-			case v := <-s.channel:
-				s.resultSlice = append(s.resultSlice, v)
+			case v := <-channel:
+				resultSlice = append(resultSlice, v)
 				wg.Done()
 			}
 		}
 	}()
 	wg.Wait()
-	return s.resultSlice
+	return resultSlice
 }
